1e46a4d4b7816442bccc3cabdf90ddaa55e3e2b6
Tsion_Mel <tsionmelese526@gmail.com>
2024-08-01 14:40:54 +0300

    Update CountPairsOfSimilarStrings.py

--- leetcode/CountPairsOfSimilarStrings.py
+++ leetcode/CountPairsOfSimilarStrings.py
@@ -2,11 +2,10 @@ class Solution:
     def similarPairs(self, words: List[str]) -> int:
         ans = 0
         cnt = Counter()
-        for i in words:
-            x = 0
-            for j in i:
-                x |= 1 << (ord(j) - ord("A"))
-            ans += cnt[x]
-            cnt[x] += 1
-        return ans
         
+        for word in words:
+            unique = ''.join(sorted(set(word)))
+            ans += cnt[unique]
+            cnt[unique] += 1
+            
+        return ans
