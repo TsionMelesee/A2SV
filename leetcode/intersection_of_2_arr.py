2163160ff612c1965366e7fd1d0d07071b2ad169
Tsion_Mel <tsionmelese526@gmail.com>
2024-08-22 19:23:08 +0300

    Update intersection_of_2_arr.py

--- leetcode/intersection_of_2_arr.py
+++ leetcode/intersection_of_2_arr.py
@@ -1,1 +1,17 @@
-
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        dic = {}
+        array = []
+        
+        for number in nums1:
+            if number not in dic:
+                dic[number] = 1
+            else:
+                dic[number] += 1
+        
+        for number in nums2:
+            if number in dic and dic[number] > 0:
+                array.append(number)
+                dic[number] -= 1
+        
+        return array
