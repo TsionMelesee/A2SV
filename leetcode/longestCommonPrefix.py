5ba1902ceca05ae2d9ccd95156d0f42d1dada424
Tsion_Mel <tsionmelese526@gmail.com>
2024-08-31 13:35:36 +0300

    Update longestCommonPrefix.py

--- leetcode/longestCommonPrefix.py
+++ leetcode/longestCommonPrefix.py
@@ -1,8 +1,15 @@
-class Solution(object):
-    def longestCommonPrefix(self, strs):
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        x = ''
         for i in range(len(strs[0])):
-            for s in strs[1:]:
-                if len(s) <= i or s[i] != strs[0][i]:
-                    return s[:i]
-        return strs[0]
+            for j in strs:
+                if i == len(j) or j[i] != strs[0][i]:
+                    return x
+             
+            x+= j[i]
+        return x
+
+        
+            
+
         
