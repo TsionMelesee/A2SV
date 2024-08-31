9bcf90188ee7c2344931801962b2eec2a6c6f1d9
Tsion_Mel <tsionmelese526@gmail.com>
2024-08-31 17:18:00 +0300

    Update goalParserInterpretation.py

--- leetcode/goalParserInterpretation.py
+++ leetcode/goalParserInterpretation.py
@@ -1,4 +1,14 @@
-class Solution(object):
-    def interpret(self, command):
-        return command.replace('()', 'o').replace('(al)', 'al')
+class Solution:
+    def interpret(self, command: str) -> str:
+        x = ''
+        for i in range(len(command)):
+            if command[i] == 'G':
+                x+='G'
+            elif command[i] == '(' and command[i + 1] != ')':
+                x+='al'
+            elif command[i] == '(' and command[i + 1] == ')':
+                x+='o'
+        return x
+
+
         
