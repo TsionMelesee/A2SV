ea31e6e1a1a792d5f4bfa94b2d36b032daa1e336
Tsion_Mel <tsionmelese526@gmail.com>
2024-09-24 18:49:37 +0300

    Update check_if_all_the_integers_im_range_are_covered.py

--- leetcode/check_if_all_the_integers_im_range_are_covered.py
+++ leetcode/check_if_all_the_integers_im_range_are_covered.py
@@ -1,12 +1,14 @@
 class Solution:
-        def isCovered(self, ranges, left, right):
-            diff = [0] * 52
-            for l, r in ranges:
-                diff[l] += 1
-                diff[r + 1] -= 1
-            s = 0
-            for i, x in enumerate(diff):
-                s += x
-                if s <= 0 and left <= i <= right:
-                    return False
-            return True
+    def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
+        x = [0] * 52
+        for l, r in ranges:
+            x[l] += 1
+            if r + 1 <= 51:
+                x[r + 1] -= 1
+        
+        s = 0
+        for i in range(1, 51):
+            s += x[i]
+            if left <= i <= right and s <= 0:
+                return False
+        return True
